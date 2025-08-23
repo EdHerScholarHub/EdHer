08f63b7818341cf2c208aa9683c248c4f58302ef
humbl3LilaS <sabishinekobebe@gmail.com>
2025-08-23 18:53:50 +0630

    Fix: remove unecessary eslint script in @repo/eslint-config package

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -11,10 +11,10 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+  // test: {
+  //   globals: true,
+  //   environment: "jsdom",
+  // },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
