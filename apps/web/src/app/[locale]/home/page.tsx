51384b00188983a105e068cd11b758b9cc377e4d
humbl3LilaS <sabishinekobebe@gmail.com>
2025-09-30 20:55:51 +0630

    Add: Locale Switcher

--- apps/web/src/app/[locale]/home/page.tsx
+++ apps/web/src/app/[locale]/home/page.tsx
@@ -1,9 +1,12 @@
 import { getTranslations } from "next-intl/server";
 
+import LocaleSelect from "../../../shared/components/locale-select";
+
 export default async function IndexPage() {
   const t = await getTranslations("HomePage");
   return (
     <div>
+      <LocaleSelect />
       <h2 className={"text-green-500 text-xl font-bold"}>{t("Title")}</h2>
     </div>
   );
---
+++ apps/web/src/shared/components/locale-select.tsx
@@ -0,0 +1,71 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@repo/ui/components/select";
+import { useLocale } from "next-intl";
+import Image from "next/image";
+
+import { useRouter } from "../../i18n/navigation";
+
+const LocaleSelect = () => {
+  const locale = useLocale();
+  const router = useRouter();
+
+  const langChange = (value: string) => {
+    if (value !== locale) {
+      router.push("/home", { locale: value });
+    }
+  };
+  return (
+    <Select onValueChange={langChange} value={locale}>
+      <SelectTrigger className={"p-3 bg-[#3e5179] text-white font-semibold"}>
+        {/* <Image */}
+        {/*  src={`/icons/${locale}-flag.png`} */}
+        {/*  alt={`${locale} flag icon`} */}
+        {/*  width={40} */}
+        {/*  height={40} */}
+        {/*  className={"w-6 aspect-square"} */}
+        {/* /> */}
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent className={"bg-[#3e5179] text-white font-semibold"}>
+        <SelectItem value={"en"}>
+          <Image
+            src={"/icons/en-flag.png"}
+            alt={"English flag icon"}
+            width={40}
+            height={40}
+            className={"w-6 aspect-square"}
+          />
+          <span>English</span>
+        </SelectItem>
+        <SelectItem value={"mm"}>
+          <Image
+            src={"/icons/mm-flag.png"}
+            alt={"Myanmar flag icon"}
+            width={40}
+            height={40}
+            className={"w-6 aspect-square"}
+          />
+          <span>Burmese</span>
+        </SelectItem>
+        <SelectItem value={"my"}>
+          <Image
+            src={"/icons/my-flag.png"}
+            alt={"Malaysia flag icon"}
+            width={40}
+            height={40}
+            className={"w-6 aspect-square"}
+          />
+          <span>Malay</span>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default LocaleSelect;
